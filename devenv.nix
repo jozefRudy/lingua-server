9cc53a3ff7be5069f35eb11e78b65193f716ea4d
jozef rudy <jozef.rudy@gmail.com>
2026-01-03 00:13:54 +0100

    use precise rust version

--- devenv.nix
+++ devenv.nix
@@ -1,24 +1,18 @@
 { pkgs, ... }:
 
 {
-
-  # https://devenv.sh/packages/
   packages = [ pkgs.git ];
-
-  languages.rust.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  languages.rust = {
+    enable = true;
+    version = "1.91.1";
+    channel = "stable";
+    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
+  };
 
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
 }
