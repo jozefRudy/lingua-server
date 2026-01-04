460fb8e2bdd4e091d21c56233474b9eb37acc207
jozef rudy <jozef.rudy@gmail.com>
2026-01-04 19:13:07 +0100

    remove coments

--- devenv.nix
+++ devenv.nix
@@ -5,13 +5,6 @@
     pkgs.git
   ];
 
-  # languages.rust = {
-  #   enable = true;
-  #   version = "1.91.1";
-  #   channel = "stable";
-  #   components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
-  # };
-
   languages.rust = {
     enable = true;
     channel = "stable";
