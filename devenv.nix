fc14a0145492d338b5a933a282c747663be61d89
jozef rudy <jozef.rudy@gmail.com>
2026-01-04 19:09:54 +0100

    simplify config

--- devenv.nix
+++ devenv.nix
@@ -5,11 +5,17 @@
     pkgs.git
   ];
 
+  # languages.rust = {
+  #   enable = true;
+  #   version = "1.91.1";
+  #   channel = "stable";
+  #   components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
+  # };
+
   languages.rust = {
     enable = true;
-    version = "1.91.1";
     channel = "stable";
-    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-src" ];
   };
+
 
 }
